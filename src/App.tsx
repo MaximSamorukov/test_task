a4f0616675af59e22054408e04d7308b7d4a87cb
maksim.samorukov <maksim.samorukov@sphaera.ru>
2025-05-22 19:58:09 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import s from "./app.module.scss";
 import { Header } from "./components/Header";
 import { Card } from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className={s.container}>
       <Header />
